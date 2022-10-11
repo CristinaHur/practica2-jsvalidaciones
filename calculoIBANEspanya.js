efcbfab67ef4e2c4d1da33f58cc10b825a6c2f53
Andr3wGC <97702693+Andr3wGC@users.noreply.github.com>
2022-10-11 10:34:50 +0200

    Add files via upload

---
+++ calculoIBANEspanya.js
@@ -0,0 +1,27 @@
+function calculoIBANEspanya(nCuenta){
+    //buscamos 24 digitos ESCCXXXXXXXXXXXXXXXXXXXX
+    //tenemos solo 20 digitos
+    //poner al final 142800 tipo XXXXXXXXXXXXXXXXXXXX142800
+    //Resto de dividir entre 97
+    //98 menos ese resto, debe dar 2 digitos, si no lo hace poner 0 a la izq  
+    //devolver ese número de control quedando como "ESXX" siendo XX los dos últimos digitos y después el resto del número de cuenta 
+
+    //Compruebo si el número de cuenta es un número 
+    if(isNaN(nCuenta)){
+        return "Número de cuenta erroneo"
+    }
+    //Lo separo y le pongo el 142800 al final, opero con él
+    let Numeros = nCuenta.split("");
+    Numeros.push(142800);
+    let nControl = parseInt(Numeros.join(""));
+    nControl = nControl%97;
+    console.log(nControl);
+    //Compruebo el resultado y lo devuelvo de forma correcta
+    if(nControl<10){
+        nControl="ES0"+nControl+nCuenta;
+        return nControl;
+    }else{
+        nControl="ES"+nControl+nCuenta;
+        return nControl;
+    }
+}
